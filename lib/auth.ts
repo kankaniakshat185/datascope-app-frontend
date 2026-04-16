b85b659cf43ac9b23695b006c08d31dcd07e3dee
Akshat Kankani <157486069+kankaniakshat185@users.noreply.github.com>
2026-04-16 18:21:30 +0530

    added tailwindcss

--- lib/auth.ts
+++ lib/auth.ts
@@ -8,7 +8,7 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-  trustedOrigins: [process.env.VERCEL_URL!],
+  trustedOrigins: ["https://data-debugger-frontend.vercel.app"],
   emailAndPassword: {
     enabled: true,
   },
